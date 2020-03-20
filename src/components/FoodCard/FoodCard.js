0a778d1292c8c00f882af03d97955a9d8eae2c82
aslnstbk <asyl.zed.han@gmail.com>
2020-03-20 19:44:40 +0600

    changed img in FoodCard.js

--- src/components/FoodCard/FoodCard.js
+++ src/components/FoodCard/FoodCard.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import s from './foodCard.module.scss';
 import loading from '../../assets/gif/preview.gif'
 
-const FoodCard = ({ title, img }) => {
+const FoodCard = ({ title, img = loading}) => {
     return (
         <div className={s.foodCard}>
             <div className={s.img}>
-                <img src={img || loading} alt='foodPhoto' />
+                <img src={img} alt='foodPhoto' />
 
                 <div className={s.overlay}/>
             </div>
